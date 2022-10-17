0b4761d62d111fdf41e8dafde5ab6b26a18470ef
Joice Pinheli <joicemagda@gmail.com>
2022-10-16 21:44:16 -0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#Automatização da instalação do Docker, Vangrant e modificação do nível de permissão do Docker Compose.
 curl -fsSL https://get.docker.com | sudo bash
 sudo curl -fsSL "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
